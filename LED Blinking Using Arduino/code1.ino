2efb2bdc471498a6fed594b3536fdba60e9a49b6
RashmiAcharya24 <acharyarashmi379@gmail.com>
2026-02-28 14:39:21 +0530

    Create code1.ino

---
+++ LED Blinking Using Arduino/code1.ino
@@ -0,0 +1,8 @@
+int ledPin = 13; // LED connected to digital pin 13 
+void setup() 
+{ pinMode(ledPin, OUTPUT);
+} 
+void loop() 
+{ 
+digitalWrite(ledPin, HIGH); // set the LED on delay(1000); // wait for a second digitalWrite(ledPin, LOW); // set the LED off delay(1000); // wait for a second
+ }
